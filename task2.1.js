8211f80164f1a75a7d01d9946b78761e4b6d378b
pregadez <pragadeesh2911@gmail.com>
2021-07-07 23:44:55 +0530

    few changes from task 2

---
+++ task2.1.js
@@ -0,0 +1,80 @@
+const form = document.getElementById('form');
+const name = document.getElementById('yourname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const phoneNumber = document.getElementById('phoneNumber');
+
+let regex = /^[\d,\s,\+,\-]{5,20}/;
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const nameValue = yourname.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	const phoneNumberValue = phoneNumber.value.trim();
+	
+	if(nameValue === '') {
+		setErrorFor(yourname, 'Name cannot be blank');
+	} else {
+		setSuccessFor(yourname);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'This field cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords don\'t match');
+	} else{
+		setSuccessFor(password2);
+	}
+
+	if(phoneNumberValue === '') {
+		setErrorFor(phoneNumber, 'Phone Number cannot be blank');
+	} else if (!isphoneNumber(phoneNumberValue)) {
+		setErrorFor(phoneNumber, 'Not a valid Phone Number');
+	} else {
+		setSuccessFor(phoneNumber);
+	}
+
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function isphoneNumber(phoneNumberValue){
+	return /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(phoneNumberValue);
+}
