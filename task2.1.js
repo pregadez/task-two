495c318bb84247014537a15fd0437c2720e21386
pregadez <pragadeesh2911@gmail.com>
2021-07-11 20:54:03 +0530

    first change

--- task2.1.js
+++ task2.1.js
@@ -4,13 +4,16 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 const phoneNumber = document.getElementById('phoneNumber');
-
+let success=0;
 let regex = /^[\d,\s,\+,\-]{5,20}/;
-
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	
 	checkInputs();
+	if(success==5){
+		$('#myModal').modal('show');
+	}
+	
 });
 
 function checkInputs() {
@@ -22,41 +25,47 @@ function checkInputs() {
 	const phoneNumberValue = phoneNumber.value.trim();
 	
 	if(nameValue === '') {
-		setErrorFor(yourname, 'Name cannot be blank');
+		setErrorFor(yourname, ' Name cannot be blank');
 	} else {
 		setSuccessFor(yourname);
+		success+=1;
 	}
 	
 	if(emailValue === '') {
-		setErrorFor(email, 'Email cannot be blank');
+		setErrorFor(email, ' Email cannot be blank');
 	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, 'Not a valid email');
+		setErrorFor(email, ' Not a valid email');
 	} else {
 		setSuccessFor(email);
+		success+=1;
 	}
 	
 	if(passwordValue === '') {
-		setErrorFor(password, 'Password cannot be blank');
+		setErrorFor(password, ' Password cannot be blank');
 	} else {
 		setSuccessFor(password);
+		success+=1;
 	}
 	
 	if(password2Value === '') {
-		setErrorFor(password2, 'This field cannot be blank');
+		setErrorFor(password2, ' This field cannot be blank');
 	} else if(passwordValue !== password2Value) {
-		setErrorFor(password2, 'Passwords don\'t match');
+		setErrorFor(password2, ' Passwords don\'t match');
 	} else{
 		setSuccessFor(password2);
+		success+=1;
+		
 	}
 
 	if(phoneNumberValue === '') {
-		setErrorFor(phoneNumber, 'Phone Number cannot be blank');
+		setErrorFor(phoneNumber, ' Phone Number cannot be blank');
 	} else if (!isphoneNumber(phoneNumberValue)) {
-		setErrorFor(phoneNumber, 'Not a valid Phone Number');
+		setErrorFor(phoneNumber, ' Not a valid Phone Number');
 	} else {
 		setSuccessFor(phoneNumber);
+		success+=1;
 	}
-
+    
 }
 
 function setErrorFor(input, message) {
@@ -78,3 +87,10 @@ function isEmail(email) {
 function isphoneNumber(phoneNumberValue){
 	return /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/.test(phoneNumberValue);
 }
+
+function form_reset(){
+	document.getElementById('form').reset();
+  }
+
+
+
